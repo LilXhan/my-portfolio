7317fbc270e7492ab78795aa1daf2ef8f3e739d6
Xhan <alvaradotuctod@gmail.com>
2022-12-11 23:29:42 -0500

    add-env

--- website/settings.py
+++ website/settings.py
@@ -11,11 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import environ
 import os
 
-env = environ.Env()
-environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,11 +85,11 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('MYSQLDATABASE'),
-        'PORT': env('MYSQLPORT'),
-        'HOST': env('MYSQLHOST'),
-        'USER': env('MYSQLUSER'),
-        'PASSWORD': env('MYSQLPASSWORD')
+        'NAME': os.environ.get('MYSQLDATABASE'),
+        'PORT': os.environ.get('MYSQLPORT'),
+        'HOST': os.environ.get('MYSQLHOST'),
+        'USER': os.environ.get('MYSQLUSER'),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD')
     }
 }
 
